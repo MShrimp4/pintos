531722ff9e119be01b7ff79bfc17df7f0274ccab
Yongha Hwang <mshrimp@sogang.ac.kr>
2021-07-07 14:13:39 +0900

    Fix attribute

--- src/threads/malloc.h
+++ src/threads/malloc.h
@@ -5,9 +5,9 @@
 #include <stddef.h>
 
 void malloc_init (void);
+void free (void *);
 void *malloc (size_t) MALLOC(free);
 void *calloc (size_t, size_t) MALLOC(free);
 void *realloc (void *, size_t);
-void free (void *);
 
 #endif /* threads/malloc.h */

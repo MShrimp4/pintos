8f39378e5999e6cf5670f4e38bc3905af90b7c12
MShrimp4 <mshrimp@sogang.ac.kr>
2021-12-30 01:53:09 +0900

    sema_up: yield only when thread system is ready

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -128,7 +128,8 @@ sema_up (struct semaphore *sema)
   sema->value++;
   intr_set_level (old_level);
 
-  thread_yield ();
+  if (old_level == INTR_ON)
+    thread_yield ();
 }
 
 static void sema_test_helper (void *sema_);
